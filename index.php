a4067a8089b6f47f737c1d0dc51182a2babf9be0
Fabian Dammekens <fabian.dammekens@cegeka.be>
2020-02-21 16:11:09 +0100

    print vars

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 echo "== SERVER";
 print_r($_SERVER);
+file_put_contents(api.log, $_SERVER, FILE_APPEND);
 echo "== GET";
 print_r($_GET);
+file_put_contents(api.log, $_GET, FILE_APPEND);
 echo "== POST";
 print_r($_POST);
+file_put_contents(api.log, $_POST, FILE_APPEND);
 echo "== INPUT";
 echo file_get_contents('php://input');
+file_put_contents(api.log, file_put_contents(api.log, $_SERVER, FILE_APPEND), FILE_APPEND);
+
+
+
 ?>
